21e5965794277365e92117703df17367eeac9db4
Joshua <satwa.fr@gmail.com>
2020-05-28 19:03:12 +0200

    barcode scanner

--- app/src/main/java/com/joshuatabakhoff/caddik/MainActivity.kt
+++ app/src/main/java/com/joshuatabakhoff/caddik/MainActivity.kt
@@ -3,21 +3,19 @@ package com.joshuatabakhoff.caddik
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-//import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     val CAMERA_PERMISSION_REQUEST_CODE = 4242
-    val IMAGE_CAPTURE_REQUEST_CODE = 1337
+    // val IMAGE_CAPTURE_REQUEST_CODE = 1337
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,21 +52,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun openCamera() {
         Toast.makeText(this, "Open camera", Toast.LENGTH_SHORT).show()
-        /*Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                startActivityForResult(takePictureIntent, IMAGE_CAPTURE_REQUEST_CODE)
-            }
-        }*/
+
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+        integrator.setPrompt("Scan a barcode")
+        integrator.setBeepEnabled(false)
+        integrator.setBarcodeImageEnabled(true)
+        integrator.setOrientationLocked(false)
+        integrator.initiateScan()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        /*if (requestCode == IMAGE_CAPTURE_REQUEST_CODE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-
-            val options = FirebaseVisionBarcodeDetectorOptions.Builder()
-                .build()
-            //imageView.setImageBitmap(imageBitmap)
-        }*/
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents !== null) {
+                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                Log.d("CADDIK", "Barcode: " + result.contents)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
