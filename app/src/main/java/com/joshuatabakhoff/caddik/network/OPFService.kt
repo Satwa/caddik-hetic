c1f772048f0ace5d5f8c19df20e0891a5c52ed73
Joshua <satwa.fr@gmail.com>
2020-05-29 16:52:14 +0200

    change how headers are sent

--- app/src/main/java/com/joshuatabakhoff/caddik/network/OPFService.kt
+++ app/src/main/java/com/joshuatabakhoff/caddik/network/OPFService.kt
@@ -3,12 +3,13 @@ package com.joshuatabakhoff.caddik.network
 import com.joshuatabakhoff.caddik.network.model.ProductResult
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 // OpenProductFacts (works for both OpenFoodFacts and OpenBeautyFacts, just switch baseUrl)
 interface OPFService {
+    @Headers("User-Agent: Caddik - Android - Version 1.0")
     @GET("product/{barcode}.json")
-    fun getProductByBarcode(@Query("barcode") barcode: String, @Header("User-Agent") ua: String = "Caddik - Android - Version 1.0"): Call<ProductResult>
+    fun getProductByBarcode(@Query("barcode") barcode: String): Call<ProductResult>
 
 }
